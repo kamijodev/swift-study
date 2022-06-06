3660e486b86c770cf5fe9f59b2ed1b82be57fa9e
kamijodev <kamijodev@gmail.com>
2022-06-06 23:44:01 +0900

    init

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+print("Hello, World!")
+
+var v = 30
+
+v = 50
+
+let c = 30
+
+print(c)
+
+print(type(of: c))
+
+// memo: xcode は shift + enterで実行
+
+// 文字列に変数の埋め込み
+if c == 30 {
+    print("\(c)歳です")
+}
+else {
+    print("\(c)歳ではないです")
+}
